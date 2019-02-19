59bc756ff086d59770df8d186766f24c15f2ec51
tsiukaris <tsioukaris@gmail.com>
2019-02-19 17:38:30 +0300

    initial project

---
+++ src/main/java/liquibase/demo/Main.java
@@ -0,0 +1,8 @@
+package liquibase.demo;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("asd");
+    }
+    //добавить liquibase.bat в PATH
+}
---
+++ src/main/java/liquibase/demo/trunk/lb_update.bat
@@ -0,0 +1,2 @@
+@echo off
+call Liquibase --changeLogFile=update.xml update
